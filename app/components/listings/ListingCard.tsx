a98c090525029a21c0fde20e5110a4884daa17e6
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-06-21 20:00:35 -0300

    safe listing

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useCountries from "@/app/hooks/UserCountries";
-import { SafeUser } from "@/app/types";
+import { SafeUser, safeListings } from "@/app/types";
 import { Listing, Reservations } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
@@ -10,7 +10,7 @@ import Image from "next/image";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 interface ListingCardPros {
-  data: Listing;
+  data: safeListings;
   reservation?: Reservations;
   onAction?: (id: string) => void;
   disable?: boolean;
