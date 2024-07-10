220c16df7b617f2878acf4a9ef935daa8143502f
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-07-10 20:18:35 -0300

    filters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,17 +2,69 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
 
-    if (userId) {
-      query.userId = userId;
+    if (userId) query.userId = userId;
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
     }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) query.locationValue = locationValue;
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              { enDate: { gte: startDate }, startDate: { lte: startDate } },
+              {
+                startDate: { lte: endDate },
+                enDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    if (category) query.category = category;
 
     const listings = await prisma.listing.findMany({
       where: query,
@@ -28,7 +80,6 @@ export default async function getListings(params: IListingParams) {
 
     return safeListings;
   } catch (err: any) {
-    console.error("Error fetching listings:", err);
     throw new Error("Failed to fetch listings. Please try again later.");
   }
 }
